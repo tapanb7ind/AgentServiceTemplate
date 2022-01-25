88367657de9bdc86a93b858d717d6e7734b16065
restlessgoose <13191067+tapanb7ind@users.noreply.github.com>
2022-01-25 14:58:23 -0500

    Add files via upload

---
+++ Services/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Services.Interface;
+using Services.Model;
+
+
+namespace Services
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _version = "1.0.0";
+            Console.WriteLine($"Starting agentService. [{_version}]");
+
+            var agent = new AgentService("Test-Execution", _version, new Tool("Locust", "2.5.0"));
+            agent.onServiceStart();
+            
+            System.Threading.Tasks.Task.Delay(1000).Wait();
+            agent.onTaskReceive(dummyTaskHandler);
+
+            System.Threading.Tasks.Task.Delay(5000).Wait();
+            agent.onStopServiceRequest();
+
+            Console.ReadLine();
+        }
+        
+        static bool dummyTaskHandler()
+        {
+            return true;
+        }       
+    }
+}
